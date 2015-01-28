424c8e992b53ed10fafe8199907988893bf4201e
Antoine Rabany <voctro@gmx.fr>
2015-01-28 19:46:03 +0100

    fin

--- rsstorrent.py
+++ rsstorrent.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+#Script téléchargeant des torrents listés dans des flux rss du fichier
+#rsslist.txt. il stoke la liste des torrents téléchargés dans downlodedlist.db
+#Ce script est fortement inspiré d'un script de maxime niankouri ecrit en perl.
+#Copyright Antoine Rabany, Maxime Niankouri
+
+#imports
 import feedparser
 import shelve
 from datetime import date
 import requests
 import os
 
+#On doit ajouter la ligne suivante dans cron pour executer le script toutes les
+#heures : crontab -e
+#15 * * * * ~/rsstorrent/rsstorrent.py
 
 def main():
   #On définit la position des fichiers
@@ -31,16 +40,16 @@ def main():
             title = entry.title
             filename = entry.torrent_filename
             torrent_hash = entry.torrent_infohash
-
+            print(url)            
             #On ouvre le shelve avec tout ce qu'on a deja telechargé
             #le hash sert de clé
             db = shelve.open(downloadedlist)
             #si le hash n'est pas présent dans la base alors le torrent n'a pas
             #été téléchargé
-            if not torrent_hash in db:
+            if torrent_hash not in db:
               #On télécharge le torrent dans le dossier watchfolder
-              os.system("curl -s --compressed -o " +watchfolder + filename + " "
-                  + url)
+              os.system("curl -s -g -L --compressed -o " +watchfolder + filename 
+                  + " " + url)
               db[torrent_hash]=[title,date.today()]
             db.close()
 
